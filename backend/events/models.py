694754496660ce16ddbc4aa9b4080f6d51290853
gulshanexcel <gulshan.excel2011@gmail.com>
2024-08-26 14:43:39 +0530

    added models

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
-# Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    reminder = models.DateTimeField(null=True, blank=True)
+    invitees = models.TextField(null=True, blank=True)
+
+    def send_invitations(self):
+        if self.invitees:
+            emails = self.invitees.split(',')
+            send_mail(
+                f'You are invited to {self.title}',
+                f'Event Details:\n\n{self.description}\n\nDate: {self.date}\nLocation: {self.location}',
+                'from@example.com',
+                emails,
+                fail_silently=False,
+            )
+
+    def __str__(self):
+        return self.title
--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Event
+from .serializers import EventSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+    def perform_create(self, serializer):
+        event = serializer.save(user=self.request.user)
+        event.send_invitations()
